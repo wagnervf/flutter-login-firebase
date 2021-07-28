cf6116b24829905fbdc760878626257164713a91
Wagner Vieira Freiria <wagner.freiria@ufms.br>
2021-07-27 22:12:15 -0400

    google sign in - initial

---
+++ lib/app/modules/login/views/google_sign_in_provider.dart
@@ -0,0 +1,78 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_loggin_firebase/app/modules/login/controllers/login_controller.dart';
+import 'package:flutter_loggin_firebase/app/modules/user/views/user_view.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleSignInProvider extends StatelessWidget {
+  final loginController = Get.put(LoginController());
+
+  // GoogleSignIn? googleSignIn;
+  GoogleSignIn googleSignIn = GoogleSignIn(
+    scopes: <String>[
+      'email',
+      'https://www.googleapis.com/auth/contacts.readonly',
+    ],
+  );
+  GoogleSignInAccount? _currentUserGoogle;
+
+  Future<void> _loginGoogle(BuildContext context) async {
+    try {
+      // googleSignIn!.signInSilently();
+      final googleUser = await googleSignIn.signIn();
+      if (googleUser == null) return;
+      _currentUserGoogle = googleUser;
+
+      final googleAuth = await googleUser.authentication;
+
+      final OAuthCredential credential = GoogleAuthProvider.credential(
+        idToken: googleAuth.idToken,
+        accessToken: googleAuth.accessToken,
+      );
+
+      loginController.setUserGoogleInFirebase(credential);
+
+      // Get.to(() => UserView());
+    } catch (error) {
+      print(error);
+      showMessage(context, 'Não foi possível realizar o login');
+    }
+  }
+
+  void showMessage(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
+  }
+
+  // Future<void> logoutGoogle() async {
+  //   await googleSignIn.disconnect();
+  //   FirebaseAuth.instance.signOut();
+  //   loginController.setLogout();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton.icon(
+          label: const Text('GOOGLE SIGN IN'),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.white,
+            onPrimary: Colors.black,
+            minimumSize: const Size(200, 50),
+          ),
+          icon: const FaIcon(
+            FontAwesomeIcons.google,
+            color: Colors.red,
+          ),
+          onPressed: () => _loginGoogle(context),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/app/modules/user/views/user_view.dart
@@ -0,0 +1,72 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_loggin_firebase/app/modules/login/controllers/login_controller.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/user_controller.dart';
+
+class UserView extends GetView<UserController> {
+  final loginController = Get.put(LoginController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('UserView'), centerTitle: true),
+      body: loggedInWidget(context),
+    );
+  }
+
+  // Widget buildStream(context) {
+  //   return StreamBuilder(
+  //       stream: FirebaseAuth.instance.authStateChanges(),
+  //       builder: (context, snapshot) {
+  //         if (snapshot.connectionState == ConnectionState.waiting) {
+  //           return const Center(child: CircularProgressIndicator());
+  //         } else if (snapshot.hasData) {
+  //           return LoggedInWidget(context);
+  //         } else if (snapshot.hasData) {
+  //           return const Center(child: Text('Erro!'));
+  //         } else {
+  //           return SignUpWidget();
+  //         }
+  //       });
+  // }
+
+  // Widget SignUpWidget() {
+  //   return Text('Default');
+  // }
+
+  Widget loggedInWidget(BuildContext context) {
+    final user = loginController.userFirebase[0];
+    final logged = loginController.userLogged;
+
+    return Center(
+      child: logged
+          ? Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * .5,
+              padding: const EdgeInsets.all(4.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  CircleAvatar(
+                      radius: 40, backgroundImage: NetworkImage(user.photoURL)),
+                  const SizedBox(height: 32),
+                  Text(user.displayName, style: TextStyle(fontSize: 20)),
+                  Text(user.email),
+                  Text('emailVerified: ${user.emailVerified.toString()}'),
+                  Text(user.uid),
+                  const SizedBox(height: 32),
+                  TextButton(
+                    onPressed: () => loginController.setLogoutAll(),
+                    child: const Text('Logout'),
+                  ),
+                ],
+              ),
+            )
+          : const Text('Not user logged in Firebase!'),
+    );
+  }
+}
